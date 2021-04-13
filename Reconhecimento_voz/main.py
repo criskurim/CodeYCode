13284373bfbeae84ac1f7bf37a3799bbecc33b5d
Romario Araujo Pinheiro <pinheiroaraujo1991@gmail.com>
2021-04-13 19:21:08 -0300

    Remoção link youtube

--- Reconhecimento_voz/main.py
+++ Reconhecimento_voz/main.py
@@ -3,12 +3,7 @@ from googlesearch import search
 import pyttsx3
 
 
-
-
-# Link vídeo com código rodando: (https://www.youtube.com/watch?v=Z1fEd-TP4zY)
-
-
-# função para ouvir o microfone
+#função para ouvir o microfone
 def ouvir_microfone():
     microfone = sr.Recognizer()
     with sr.Microphone() as source:
@@ -21,7 +16,7 @@ def ouvir_microfone():
             return "Não entendi!"
 
 
-# função para sintetizar voz
+#função para sintetizar voz
 def sintese_voz(entrada_de_texto):
     falar = pyttsx3.init('sapi5')
     falar.say(entrada_de_texto)
@@ -30,34 +25,33 @@ def sintese_voz(entrada_de_texto):
 
 
 
-
-# loop para ouvir continuamente
+#loop para ouvir continuamente
 ouvindo = True
 while ouvindo:
     bet_diz = 'Diga algo!'
-    print(bet_diz)  # solicita que o usuária diga algo
+    print(bet_diz)  #solicita que o usuária diga algo
     sintese_voz(bet_diz)
-    ouvir_microfone()  # aciona a função ouvir_microfone()
-    frase = ouvir_microfone()  # armazena em uma variável o que foi retornado da função
+    ouvir_microfone()  #aciona a função ouvir_microfone()
+    frase = ouvir_microfone()  #armazena em uma variável o que foi retornado da função
     frase = frase.lower()
-    if frase == "poderia me ajudar":  # condição para entrar o comando pesquisar
+    if frase == "poderia me ajudar":  #condição para entrar o comando pesquisar
         bet_diz = ("O que deseja pesquisar? ")
         print(bet_diz)
         sintese_voz(bet_diz)
-        busca = ouvir_microfone()  # armazena na variável busca o que foi retornado da função ouvir_microfone
+        busca = ouvir_microfone()  #armazena na variável busca o que foi retornado da função ouvir_microfone
         bet_diz = (f'Estes são os resultados para: {busca}')
         sintese_voz(bet_diz)
-        print(f'{bet_diz}')  # confirma para o usuário o que foi dito
-        for i in search(busca, tld="com", num=4, stop=4, pause=2):  # realiza a pesquisa
+        print(f'{bet_diz}')  #confirma para o usuário o que foi dito
+        for i in search(busca, tld="com", num=4, stop=4, pause=2):  #realiza a pesquisa
             print (i)
 
 
-    elif frase == "obrigado":  # comando de voz que encerra o programa
+    elif frase == "obrigado":  #comando de voz que encerra o programa
         bet_diz = "Foi um prazer ajudá-lo!!"
         sintese_voz(bet_diz)
         print(bet_diz)
         ouvindo = False
-    else:  # imprime sempre que não atende a nenhuma condição
+    else:  #imprime sempre que não atende a nenhuma condição
         bet_diz = (frase)
         sintese_voz(bet_diz)
         print(bet_diz)
